bd21f7b6a8bee8141c0ee2044059e7a0c4632b31
jinsang park <jsgd1254@gmail.com>
2024-11-26 13:57:21 +0900

    Lv.3 개발 - CurrencyRequestDto Validation 추가 - GlobalExceptionHandler 추가 및 적용

--- src/main/java/com/sparta/currency_exchange/dto/CurrencyRequestDto.java
+++ src/main/java/com/sparta/currency_exchange/dto/CurrencyRequestDto.java
@@ -1,13 +1,16 @@
 package com.sparta.currency_exchange.dto;
 
 import com.sparta.currency_exchange.entity.Currency;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 import java.math.BigDecimal;
 
 @Getter
 public class CurrencyRequestDto {
+    @NotNull(message = "통화 이름은 필수입니다.")
     private String currencyName;
+    @NotNull(message = "환율 필수입니다.")
     private BigDecimal exchangeRate;
     private String symbol;
 
